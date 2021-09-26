bcb075836b39c745d218c5b9156dba3940832390
Prashasthi K Kanchan <75254746+prashasthikanchan@users.noreply.github.com>
2021-09-26 22:57:40 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,31 +1,17 @@
-# Import required libraries
 import pandas as pd
-import numpy as np 
-import matplotlib.pyplot as plt
-import sklearn
-
-# Import necessary modules
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from math import sqrt
-
-# Keras specific
-import keras
+data = pd.read_csv('diabetes.csv')
+print(data)
+x = data.drop("Outcome", axis=1)
+y = data["Outcome"]
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.utils import to_categorical 
-X = [[30],[40],[50],[60],[20],[10],[70]]
-y = [0,1,1,1,0,0,1]
 model = Sequential()
-model.add(Dense(500, activation='relu', input_dim=1))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(50, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-
-# Compile the model
-model.compile(optimizer='adam', 
-              loss='binary_crossentropy', 
-              metrics=['accuracy'])
-model.fit(X,y, epochs=100)
-X_marks=[[20]]
-print(model.predict(X_marks))
+model.add(Dense(12, input_dim=8, activation="relu"))
+model.add(Dense(12, activation="relu"))
+model.add(Dense(1, activation="sigmoid"))
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.fit(x,y, epochs=150, batch_size=10)
+_, accuracy = model.evaluate(x, y)
+print("Model accuracy: %.2f"% (accuracy*100))
+predictions = model.predict(x)
+print([round(x[0]) for x in predictions])
